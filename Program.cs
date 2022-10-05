4904849e6b0337d022163f257587c1669a6b469d
Elena <ea.alipova80@gmail.com>
2022-10-05 20:55:03 +0300

    Add homework2 file

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//int number = new Random().Next(100,999);
+//Console.WriteLine($"Введите трехзначное число: {number}");
+//int num1 = number/100; 
+//int num2 = number%10;
+//int num3 = number%10;
+
+
+//Console.WriteLine($"{number}, {num2}");
+
+
+
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+/*
+int number = new Random().Next(0,100000);
+Console.WriteLine("Введите number: ");
+if (number >=100 && number <=999)
+{
+    Console.WriteLine($"Третья цифра числа {number%10}");
+}
+if (number >=1000 && number <=9999)
+{
+    Console.WriteLine($"Третья цифра числа {number/10%10}");
+}
+if (number >=10000 && number <=99999)
+{
+    Console.WriteLine($"Третья цифра числа {number/100%10}");
+}
+else if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+*/
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. Обязательно сделать проверку на ввод чисел меньше 1 и больше 7
+/*
+ Console.WriteLine("Введите день недели");
+ int DayOff = int.Parse(Console.ReadLine()!);
+
+ if (DayOff >=1 && DayOff <=7)
+ 
+ if (DayOff == 6 || DayOff == 7)
+ {
+Console.WriteLine("Ввод DayOff");
+ }
+ else
+ {
+Console.WriteLine("Ввод будний день");
+ }
+ */
+ 
